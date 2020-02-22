5cc2db75ec49900cc89925fe64d9e1fc89af2cd7
Harrison Cutler <harrisoncutler5@gmail.com>
2020-02-22 17:33:31 +0000

    Updated the home view to include the new components.

--- to-do-list/src/views/Home.vue
+++ to-do-list/src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="navbar">
-    <navbar />
+  <div class="home">
+    <div class="navbar">
+      <navbar />
+    </div>
+    <div class="action-bar">
+      <action-bar />
+    </div>
+    <br>
+    <div class="todo">
+      <todo />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import navbar from '@/components/navbar/navbar.vue';
+import todo from '@/components/to-do-list/to-do-list.vue';
+import actionBar from '@/components/action-bar/action-bar.vue';
 
 export default {
   name: 'Home',
   components: {
     navbar,
+    todo,
+    actionBar,
   },
 };
 </script>
