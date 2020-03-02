7070cbd568037ac2cecb7798d007c54734e5af3f
Harrison Cutler <harrisoncutler5@gmail.com>
2020-03-02 19:49:43 +0000

    Added validation to the task name field.

--- to-do-list/src/components/to-do-list/new-todo.vue
+++ to-do-list/src/components/to-do-list/new-todo.vue
@@ -1,63 +1,80 @@
 <template>
   <div class="new-todo">
     <div class="task-details">
-      <b-field label="Name" :message="isValid() ? null : 'Name is required'" :type="{ 'is-danger': !isValid() }">
-        <b-input
-          class="detail-input todo-name-input"
-          placeholder="Enter a name for the task"
+      <ValidationObserver v-slot="{ handleSubmit }">
+        <form>
+          <validationProvider name="name" rules="required" v-slot="{ errors }">
+            <b-field
+              :type="{ 'is-danger': errors[0] }"
+              :message="errors"
+            >
+              <b-input
+                class="detail-input todo-name-input"
+                placeholder="Enter a name for the task"
+                rounded
+                v-model="name"
+              >
+              </b-input>
+            </b-field>
+          </validationProvider>
+          <b-input
+            class="detail-input todo-description-input"
+            placeholder="Enter a description for the task"
+            rounded
+            v-model="description"
+          >
+          </b-input>
+          <b-switch
+            class="is-required"
+            v-model="required"
+          >
+            Is this a required task?
+          </b-switch>
+        </form>
+        <b-button
           rounded
-          v-model="name"
+          class="submit"
+          size="is-small"
+          @click="handleSubmit(addTodo)"
         >
-        </b-input>
-      </b-field>
-      <b-input
-        class="detail-input todo-description-input"
-        placeholder="Enter a description for the task"
-        rounded
-        v-model="description"
-      >
-      </b-input>
-      <b-switch
-        class="is-required"
-        v-model="required"
-      >
-        Is this a required task? {{ required }}
-      </b-switch>
+          Submit
+        </b-button>
+      </ValidationObserver>
     </div>
-    <b-button
-      rounded
-      class="submit"
-      size="is-small"
-      @click="addTodo"
-    >
-      Submit
-    </b-button>
   </div>
 </template>
 
 <script lang="ts">
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate';
+import { required } from 'vee-validate/dist/rules';
 import { Vue, Component, Prop } from 'vue-property-decorator';
+// @ts-ignore
+import { messages } from 'vee-validate/dist/locale/en.json';
 
-@Component
+@Component({
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
+})
 
 export default class newTodo extends Vue {
   private name: string | null = null;
   private description: string | null = null;
   private required: boolean = false;
 
-  private isValid(): boolean {
-    if (this.name === '' || null) {
-      return false;
-    };
-    return true;
-  }
-
   private addTodo(): void {
-    if (this.isValid()) return;
     this.$emit('todoAdded', this.name, this.description, this.required);
     this.name = null;
     this.description = null;
     this.required = false;
+  }
+
+  private mounted(): void {
+    extend('required', {
+      ...required,
+      message: messages['required'],
+    });
   }
 }
 
